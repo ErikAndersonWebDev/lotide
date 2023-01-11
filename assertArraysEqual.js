a654e3f2d437d6781509e9b78ef4389855069fce
Erik Anderson <108945295+ErikAndersonWebDev@users.noreply.github.com>
2023-01-11 11:32:53 -0800

    updated assertequalarrays function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,4 @@
-const assertArraysEqual = function(arrayOne, arrayTwo) {
+/* const assertArraysEqual = function(arrayOne, arrayTwo) {
   if ((eqArrays(arrayOne, arrayTwo) === true)) {
     console.log(`\u2705\u2705\u2705 Arrays are equal`);
   } else {
@@ -18,4 +18,27 @@ const eqArrays = function(array1, array2) {
     };
   };
   return true;
+}; */
+
+const eqArrays = function(array1, array2) {
+  if (array1.length != array2.length) {
+    return false;
+  };
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      continue;
+    } else {
+      return false;
+    };
+  }
+  return true;
 };
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705\u2705\u2705Arrays are equal`);
+  } else {
+    console.log(`\u26D4\u26D4\u26D4Arrays are NOT equal`);
+  }
+};
+
--- flatten.js
+++ flatten.js
@@ -1,11 +1,3 @@
-const assertArraysEqual = function(arrayOne, arrayTwo) {
-  if (eqArrays(arrayOne, arrayTwo)) {
-    console.log(`\u2705\u2705\u2705 Arrays are equal`);
-  } else {
-    console.log(`\u26D4\u26D4\u26D4 Arrays are NOT equal`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   if (array1.length != array2.length) {
     return false;
@@ -16,9 +8,19 @@ const eqArrays = function(array1, array2) {
     } else {
       return false;
     };
-  };
+  }
   return true;
 };
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705\u2705\u2705Arrays are equal`);
+  } else {
+    console.log(`\u26D4\u26D4\u26D4Arrays are NOT equal`);
+  }
+};
+
+
 
 const flatten = function(nestedArray) {
   let flatArray = [];
