229ecf560a0323fbfe04b6d2de93096ca1848c57
Erik Anderson <108945295+ErikAndersonWebDev@users.noreply.github.com>
2023-01-11 14:24:59 -0800

    re-ordered functions

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,44 +1,22 @@
-/* const assertArraysEqual = function(arrayOne, arrayTwo) {
-  if ((eqArrays(arrayOne, arrayTwo) === true)) {
-    console.log(`\u2705\u2705\u2705 Arrays are equal`);
-  } else {
-    console.log(`\u26D4\u26D4\u26D4 Arrays are NOT equal`);
-  }
-};
-
-const eqArrays = function(array1, array2) {
-  if (array1.length != array2.length) {
-    return false;
-  };
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      continue;
-    } else {
-      return false;
-    };
-  };
-  return true;
-}; */
-
-const eqArrays = function(array1, array2) {
-  if (array1.length != array2.length) {
-    return false;
-  };
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      continue;
-    } else {
-      return false;
-    };
-  }
-  return true;
-};
-
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`\u2705\u2705\u2705Arrays are equal`);
   } else {
     console.log(`\u26D4\u26D4\u26D4Arrays are NOT equal`);
   }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length != array2.length) {
+    return false;
+  };
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      continue;
+    } else {
+      return false;
+    };
+  }
+  return true;
 };
 
