8e66437a3189de132dd39bd13e0fb390cce0de13
Erik Anderson <108945295+ErikAndersonWebDev@users.noreply.github.com>
2023-01-10 18:34:27 -0800

    created assrtArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo)) {
+    console.log(`\u2705\u2705\u2705 Arrays are equal`);
+  } else {
+    console.log(`\u26D4\u26D4\u26D4 Arrays are NOT equal`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length != array2.length) {
+    return false;
+  };
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      continue;
+    } else {
+      return false;
+    };
+  };
+  return true;
+};
