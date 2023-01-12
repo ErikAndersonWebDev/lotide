ef97e9ddba5aa050b317177d64b8d521fe41c8d4
Erik Anderson <108945295+ErikAndersonWebDev@users.noreply.github.com>
2023-01-11 19:45:31 -0800

    added findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705\u2705\u2705Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u26D4\u26D4\u26D4Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (obj, value) {
+  for (let genre in obj) {
+    if (obj[genre] === value){
+      return genre;
+    };
+  };
+  return undefined;
+};
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
